e34b9e0a82fb2666b416382488f423edc4ce7d9a
wasd <wasdsweden@gmail.com>
2016-10-20 19:38:22 +0200

    Add some future features

---
+++ src/com/wasd/lib3d/future_features/Banner.java
@@ -0,0 +1,12 @@
+package com.wasd.lib3d.future_features;
+
+import com.wasd.lib3d.shapes.Shape;
+
+public class Banner extends Shape {
+
+    //TODO should look like this https://www.google.co.uk/search?q=opengl+3d+text&tbm=isch
+
+    public Banner() {
+        super(-1, -1);
+    }
+}
